46060b93822995a0a2db5dcbcf8d59c58342623e
Sergey Petrov <mavericusdev@gmail.com>
2021-01-28 19:58:36 +0300

    реализует класс Car (синтаксист ES6)

---
+++ index.js
@@ -0,0 +1,8 @@
+import CarEs6 from './src/car.js6.js';
+
+const carEs6 = new CarEs6();
+
+carEs6.ride();
+carEs6.stop();
+carEs6.turnRight();
+carEs6.turnLeft();
---
+++ src/car.js6.js
@@ -0,0 +1,19 @@
+export default class Car {
+    ride(){
+        console.log(`ride`);
+    }
+
+    stop(){
+        console.log(`stop`);
+    }
+
+
+    turnRight(){
+        console.log(`ride`);
+    }
+
+
+    turnLeft(){
+        console.log(`turnLeft`);
+    }
+}
